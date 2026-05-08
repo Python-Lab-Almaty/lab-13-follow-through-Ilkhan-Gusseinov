8dd5d544d3d11e286c42a470e0510d2fb632d1fd
Ilkhan-Gusseinov <ilhan.guseynov@gmail.com>
2026-05-08 15:51:43 +0500

    Task 5: Add life system

--- game.py
+++ game.py
@@ -156,6 +156,7 @@ dynamic_obstacles = []
 # ----------------------------
 steps = 0
 penalties = 0
+life = 3 
 
 # ----------------------------
 # 🟢 СКОРОСТЬ
@@ -271,7 +272,7 @@ def draw_all():
     score_drawer.goto(0, -HEIGHT//2 + 40)
     score = steps - penalties
     score_drawer.clear()
-    score_drawer.write(f"Steps: {steps} | Penalties: {penalties} | Score: {score}",
+    score_drawer.write(f"Steps: {steps} | Life: {life} | Penalties: {penalties} | Score: {score}",
                        align="center", font=("Arial", 16, "bold"))
     
     screen.update()
@@ -287,7 +288,7 @@ def rect_collision(hero_x, hero_y, rect_x, rect_y, rect_w, rect_h, hero_radius=1
 
 def check_collision():
     """Проверяет столкновения"""
-    global penalties
+    global penalties, life
     for ox, oy, (w, h) in impassable_obstacles:
         if rect_collision(hero.xcor(), hero.ycor(), ox, oy, w, h, hero_radius=15):
             penalties += 10
@@ -298,7 +299,10 @@ def check_collision():
     for obs in dynamic_obstacles:
         x, y, w, h, is_falling, speed, final_y = obs
         if rect_collision(hero.xcor(), hero.ycor(), x, y, w, h, hero_radius=15):
-            return "game_over"
+            life -= 1
+            hero.goto(hero.xcor() - vx*3, hero.ycor() - vy*3)
+            if life <= 0:
+                return "game_over"
     
     return "ok"
 
